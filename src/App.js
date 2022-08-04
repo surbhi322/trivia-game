50bc8539d454663a40d5644ac68241e837d01e18
surbhi <surbhi1997l@gmail.com>
2022-08-04 16:21:00 +0530

    trivia game

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Question from './components/Question';
+
+function App() {
+  return (
+    <div className="App">
+      <Question/>
+    </div>
+  );
+}
+
+export default App;
